16c24399e4f04f86c0287b14d1a541c91ba66cd2
Angel Huayas <angelhuayas@gmail.com>
2021-03-29 10:26:18 -0500

    fix following bug

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :commentable, polymorphic: true
+  belongs_to :commentable, polymorphic: true , counter_cache: :comments_count
   has_many :likes , as: :likeable
   has_many :comments ,as: :commentable
 
