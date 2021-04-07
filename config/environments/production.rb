575dd864f6fce4b759ec527e77d83e1f08ca2883
Angel Huayas <angelhuayas@gmail.com>
2021-04-07 15:02:09 -0500

    add mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,15 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "wetravel_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            'we.travel.capstone@gmail.com',
+    password:             'evqviccsjrdqzimo',
+    authentication:       'plain',
+    enable_starttls_auto: true }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
